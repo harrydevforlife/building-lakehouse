073b00ea92b805e5b3f292ad5bd9481f01170c14
ManhTCT <manhtct@DESKTOP-2RB2ROF>
2023-07-02 00:11:01 +0700

    edit

---
+++ restaurant_analytis/target/compiled/restaurant_analytis/models/gold/transform/analyses_review.sql
@@ -0,0 +1,20 @@
+WITH analyses AS (
+    SELECT 
+        fr.*,
+        dr.restaurant_name as restaurant_name,
+        dr.categories as categories,
+        dr.city as city,
+        dr.latitude as latitude,
+        dr.longitude as longitude,
+        dr.stars as restaurant_stars,
+        dr.state as state,
+        dr.review_count as review_count,
+        dr.hours as hours,
+        du.user_name
+    FROM dev_silver.fact_review fr
+    LEFT JOIN dev_silver.dim_restaurant dr
+    ON fr.restaurant_id = dr.restaurant_id
+    LEFT JOIN dev_silver.dim_user du 
+    ON fr.user_id = du.user_id
+)
+SELECT * FROM analyses
---
+++ restaurant_analytis/target/run/restaurant_analytis/models/silver/transform/dim_user.sql
@@ -0,0 +1,28 @@
+create or replace view dev_silver.dim_user
+  
+  as
+    WITH user_raw AS (
+    SELECT 
+        us.user_id as user_id,
+        us.name as user_name,
+        us.average_stars as average_stars,
+        us.review_count as review_count,
+        us.funny as funny,
+        us.cool as cool,
+        us.fans as fans,
+        us.friends as friends,
+        us.compliment_cool as compliment_cool,
+        us.compliment_cute as compliment_cute,
+        us.compliment_funny as compliment_funny,
+        us.compliment_hot as compliment_hot,
+        us.compliment_list as compliment_list,
+        us.compliment_more as compliment_more,
+        us.compliment_note as compliment_note,
+        us.compliment_photos as compliment_photos,
+        us.compliment_plain as compliment_plain, 
+        us.compliment_profile as compliment_profile,
+        us.compliment_writer as compliment_writer
+    FROM bronze.user us
+)
+
+SELECT * FROM user_raw
