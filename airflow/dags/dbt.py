a6317de21f6b69a00fe0e739fc9815c884f06069
ManhTCT <manhtct@DESKTOP-2RB2ROF>
2023-07-03 01:56:40 +0700

    edit dbt run job

--- airflow/dags/dbt.py
+++ airflow/dags/dbt.py
@@ -7,15 +7,15 @@ from airflow.operators.bash import BashOperator
 from datetime import datetime, timedelta
 
 def set_env():
-    print("Hello World")
-    os.environ["DBT_PROFILES_DIR"] = os.getcwd() + "/restaurant_analytis/"
-    os.environ["DBT_PROJECT_DIR"] = os.getcwd() + "/restaurant_analytis/"
+    print("Setting dbt environment variables")
+    os.environ["DBT_PROFILES_DIR"] = "$AIRFLOW_HOME/restaurant_analytis"
+    os.environ["DBT_PROJECT_DIR"] = "$AIRFLOW_HOME/restaurant_analytis"
 
 with DAG(dag_id="run_dbt", start_date=datetime(2020, 1, 1), schedule_interval="@daily", catchup=False) as dag:
     set_environment = PythonOperator(task_id="hello_world", python_callable=set_env)
     run_dbt = BashOperator( 
         task_id="run_dbt",
-        bash_command="dbt run",
+        bash_command="dbt run --profiles-dir $DBT_PROFILES_DIR --project-dir $DBT_PROJECT_DIR",
         env={"SPARK_HOME": "/usr/local/airflow/jars"}
     )
 
